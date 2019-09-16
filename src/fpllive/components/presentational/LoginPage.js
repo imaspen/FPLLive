7904b790021b69947c3beccf575e7369239f7057
Aspen <zebington@gmail.com>
2019-09-16 22:29:22 +0100

    Added styling to login page, updated to paper v 3 alpha 5

--- src/fpllive/components/presentational/LoginPage.js
+++ src/fpllive/components/presentational/LoginPage.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import {Button, TextInput} from 'react-native-paper';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 
-export default props => (
-    <View style={{flex: 1}}>
-        <TextInput label="Email" value={props.email} onChangeText={props.setEmail} textContentType="username" keyboardType="email-address" autoCapitalize="none"/>
-        <TextInput label="Password" value={props.password} onChangeText={props.setPassword} textContentType="password" secureTextEntry={true} />
-        <Button onPress={props.login}>Log In</Button>
-    </View>
+const MyTextInput = props => (
+    <TextInput
+        {...props} style={styles.input} mode="outlined" allowFontScaling={false}
+    />
 );
+
+export default props => {
+    DeviceInfo.getFontScale().then(fontScale => {
+        // styles.input.paddingBottom = 16 / fontScale;
+        console.log(16 / fontScale);
+    });
+
+    return (
+        <View style={styles.view}>
+            <MyTextInput label="Email" value={props.email} onChangeText={props.setEmail} textContentType="username"
+                         keyboardType="email-address" autoCapitalize="none" placeholder="Email"/>
+            <MyTextInput label="Password" value={props.password} onChangeText={props.setPassword}
+                         textContentType="password" secureTextEntry={true}/>
+            <Button onPress={props.login} style={styles.input}>Log In</Button>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        padding: 10,
+    },
+    input: {
+        margin: 10,
+    },
+});
